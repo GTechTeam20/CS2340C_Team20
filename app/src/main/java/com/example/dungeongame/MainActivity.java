25dcf9ee42c58d00660d74d09cb67d8a554b5ea6
Jon Liu <jonliu03@gmail.com>
2023-09-24 16:17:00 -0400

    Redid project

---
+++ app/src/main/java/com/example/dungeongame/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.dungeongame;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.view.WindowManager;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.dungeongame.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private Button btnSelectDifficulty;
+    private Button btnStartGame;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSelectDifficulty = findViewById(R.id.btnSelectDifficulty);
+        btnStartGame = findViewById(R.id.btnStartGame);
+
+        btnSelectDifficulty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle difficulty selection logic here (e.g., show a dialog or another activity).
+            }
+        });
+
+        btnStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the game by transitioning to a blank screen (you need to create this activity).
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public void startGame(View view) {
+
+    }
+}
