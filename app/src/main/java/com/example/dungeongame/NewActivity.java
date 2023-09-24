9c9b404c0eaccf401862f13cbcd8b30429cb9604
Tamas Mester <tamascmester@gmail.com>
2023-09-24 19:29:49 -0400

    Added exit button, player select logic, and input check

--- app/src/main/java/com/example/dungeongame/NewActivity.java
+++ app/src/main/java/com/example/dungeongame/NewActivity.java
@@ -1,10 +1,12 @@
 package com.example.dungeongame;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -14,10 +16,11 @@ import androidx.appcompat.app.AppCompatActivity;
 public class NewActivity extends AppCompatActivity {
 
     private Button btnContinue;
-    private ImageView character1, character2, character3;
+    private ImageButton character1, character2, character3;
     private String selectedCharacter = "";
     private EditText etPlayerName;
     private RadioGroup radioGroupDifficulty;
+    private ImageButton selectedImageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +42,7 @@ public class NewActivity extends AppCompatActivity {
             public void onClick(View v) {
                 selectedCharacter = "monkey";
                 btnContinue.setEnabled(true);
+                onImageButtonClick(character1);
             }
         });
 
@@ -47,6 +51,7 @@ public class NewActivity extends AppCompatActivity {
             public void onClick(View v) {
                 selectedCharacter = "pickle";
                 btnContinue.setEnabled(true);
+                onImageButtonClick(character2);
             }
         });
 
@@ -55,6 +60,7 @@ public class NewActivity extends AppCompatActivity {
             public void onClick(View v) {
                 selectedCharacter = "banana";
                 btnContinue.setEnabled(true);
+                onImageButtonClick(character3);
             }
         });
 
@@ -62,22 +68,31 @@ public class NewActivity extends AppCompatActivity {
         btnContinue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int selectedRadioButtonId = radioGroupDifficulty.getCheckedRadioButtonId();
-                RadioButton selectedRadioButton = findViewById(selectedRadioButtonId);
-                String selectedDifficulty = selectedRadioButton.getText().toString();
-                Intent intent = new Intent(NewActivity.this, GameActivity.class);
-                intent.putExtra("selectedCharacter", selectedCharacter);
-                intent.putExtra("playerName", etPlayerName.getText().toString().trim());
-                intent.putExtra("selectedDifficulty", selectedDifficulty);
-                startActivity(intent);
+                if (isInputValid(etPlayerName.getText().toString())) {
+                    int selectedRadioButtonId = radioGroupDifficulty.getCheckedRadioButtonId();
+                    RadioButton selectedRadioButton = findViewById(selectedRadioButtonId);
+                    String selectedDifficulty = selectedRadioButton.getText().toString();
+                    Intent intent = new Intent(NewActivity.this, GameActivity.class);
+                    intent.putExtra("selectedCharacter", selectedCharacter);
+                    intent.putExtra("playerName", etPlayerName.getText().toString().trim());
+                    intent.putExtra("selectedDifficulty", selectedDifficulty);
+                    startActivity(intent);
+                }
             }
         });
     }
 
     // Implement validation logic for name input and difficulty selection.
 
-    private boolean isInputValid() {
-        // Implement validation logic and return true if everything is valid, false otherwise.
-        return true;
+    private boolean isInputValid(String input) {
+        return input != null && !input.trim().isEmpty();
+    }
+
+    public void onImageButtonClick(ImageButton imageButton) {
+        if (selectedImageButton != null) {
+            selectedImageButton.setColorFilter(Color.TRANSPARENT); // Reset the tint
+        }
+        imageButton.setColorFilter(getResources().getColor(R.color.purple_500));
+        selectedImageButton = imageButton;
     }
 }
