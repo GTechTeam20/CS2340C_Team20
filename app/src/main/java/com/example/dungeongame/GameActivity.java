284a51f6b06e9bc36e9a3b7e886f52e579d7e374
Jon Liu <chunleiliu@lawn-10-91-52-200.lawn.gatech.edu>
2023-09-24 18:13:26 -0400

    Added config changes and implemented screen flow

--- app/src/main/java/com/example/dungeongame/GameActivity.java
+++ app/src/main/java/com/example/dungeongame/GameActivity.java
@@ -1,18 +1,26 @@
 package com.example.dungeongame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
-
+    private static final long DELAY_MILLIS = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        TextView textView = findViewById(R.id.textViewGameMessage);
-        textView.setText("Not implemented yet");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(GameActivity.this, EndScreen.class);
+                startActivity(intent);
+                finish(); // Finish this activity to prevent going back to it.
+            }
+        }, DELAY_MILLIS);
     }
 }
