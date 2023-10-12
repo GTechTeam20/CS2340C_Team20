f64bf84397427dac62678698d9d473a0c18d20ff
Jon Liu <chunleiliu@Jons-Mac.local>
2023-10-11 23:57:05 -0400

    Implemented 3 rooms, unit testing for difficulty selector

---
+++ app/src/test/java/com/example/dungeongame/GameActivityUnitTests.java
@@ -0,0 +1,43 @@
+package com.example.dungeongame;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.dungeongame.views.GameActivity;
+
+public class GameActivityUnitTests {
+    private GameActivity gameActivity;
+
+    @Before
+    public void setUp() {
+        gameActivity = new GameActivity(); // Instantiate the activity
+    }
+
+    @Test
+    public void testStartingHealthForEasyDifficulty() {
+        // Call the method that sets starting health for "Easy" difficulty
+        int startingHealth = gameActivity.getStartingHealthForDifficulty("Easy");
+
+        // Verify that the starting health is set to 100 for "Easy" difficulty
+        assertEquals(100, startingHealth);
+    }
+
+    @Test
+    public void testStartingHealthForMediumDifficulty() {
+        // Call the method that sets starting health for "Medium" difficulty
+        int startingHealth = gameActivity.getStartingHealthForDifficulty("Medium");
+
+        // Verify that the starting health is set to 75 for "Medium" difficulty
+        assertEquals(75, startingHealth);
+    }
+
+    @Test
+    public void testStartingHealthForHardDifficulty() {
+        // Call the method that sets starting health for "Hard" difficulty
+        int startingHealth = gameActivity.getStartingHealthForDifficulty("Hard");
+
+        // Verify that the starting health is set to 50 for "Hard" difficulty
+        assertEquals(50, startingHealth);
+    }
+}
