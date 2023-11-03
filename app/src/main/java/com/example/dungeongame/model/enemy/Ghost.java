5d80ceb9d224f4f70a78d7f61a4410653995a3bd
sbhatia84 <143375332+sbhatia84@users.noreply.github.com>
2023-11-03 04:42:00 -0400

    Update Ghost.java

--- app/src/main/java/com/example/dungeongame/model/enemy/Ghost.java
+++ app/src/main/java/com/example/dungeongame/model/enemy/Ghost.java
@@ -1,32 +1,61 @@
 package com.example.dungeongame.model.enemy;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Ghost implements Enemy {
     private int damage = 2;
     private String sprite = "Ghost";
     private int movementSpeed = 3;
     private int x;
     private int y;
+
+    private Timer timer;
+    private TimerTask timerTask;
+
+    public Ghost() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            private int direction = 1;  // 1 for moving up, -1 for moving down
+
+            @Override
+            public void run() {
+                move(direction);
+                direction *= -1;  // Toggle direction between up and down
+            }
+        }, 0, 1000);  // Adjust the delay (1000ms = 1 second) as needed
+    }
     @Override
     public void attackPlayer(){
         //attack the player
     }
 
+    private void moveHelper(int direction) {
+        y += movementSpeed * direction;
+    }
     @Override
-    public void move(String Direction) {
-        if (Direction.equals("Left")) {
-            x = x - movementSpeed;
-        }
+    public void move(int direction) {
+        y += movementSpeed * direction;
 
-        if (Direction.equals("Right")) {
-            x = x + movementSpeed;
-        }
+        //while ()
+        //if (Direction.equals("Left")) {
+        //    x = x - movementSpeed;
+        //}
 
-        if (Direction.equals("Up")) {
-            y = y + movementSpeed;
-        }
+        //if (Direction.equals("Right")) {
+        //    x = x + movementSpeed;
+        //}
 
-        if (Direction.equals("Down")) {
-            y = y - movementSpeed;
-        }
+        //if (Direction.equals("Up")) {
+        //    y = y + movementSpeed;
+        //}
+
+        //if (Direction.equals("Down")) {
+        //    y = y - movementSpeed;
+        //}
+    }
+
+    public static Ghost createGhost() {
+        return new Ghost();
     }
 }
