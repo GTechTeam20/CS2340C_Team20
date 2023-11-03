1695206d0d7c5650fd2e5b3b360f587d5ba12fce
emilysun1160 <emilysun1160@gmail.com>
2023-11-03 01:43:55 -0400

    updated ghost class again

--- app/src/main/java/com/example/dungeongame/model/enemy/Ghost.java
+++ app/src/main/java/com/example/dungeongame/model/enemy/Ghost.java
@@ -1,16 +1,28 @@
 package com.example.dungeongame.model.enemy;
 
+import android.media.Image;
+import android.widget.ImageView;
+
 public class Ghost implements Enemy {
     private int damage = 2;
     private String sprite = "Ghost";
     private int movementSpeed = 3;
     private int x;
     private int y;
+
+    private ImageView ghostImage;
     @Override
     public void attackPlayer(){
         //attack the player
     }
 
+    public Ghost(int x, int y, ImageView ghostImage) {
+        this.x = x;
+        this.y = y;
+        this.ghostImage = ghostImage;
+        ghostImage.setX(x);
+        ghostImage.setY(y);
+    }
     @Override
     public void move(String Direction) {
         if (Direction.equals("Left")) {
