5f4e3988f4120f5aef34b9d0897588bc58125542
Emily Yang <emilysun1160@gmail.com>
2023-11-28 21:02:56 -0500

    unit tests added

--- app/src/test/java/com/example/dungeongame/Sprint5UnitTests.java
+++ app/src/test/java/com/example/dungeongame/Sprint5UnitTests.java
@@ -3,6 +3,7 @@ package com.example.dungeongame;
 import static org.junit.Assert.assertEquals;
 
 import com.example.dungeongame.model.Player;
+import com.example.dungeongame.model.Sword;
 import com.example.dungeongame.model.collisions.CollisionManager;
 import com.example.dungeongame.model.enemy.Crab;
 import com.example.dungeongame.model.enemy.Ghost;
@@ -18,6 +19,8 @@ public class Sprint5UnitTests {
     Heart heart = Heart.getInstance(200, 300, "heart");
     Coin coin = Coin.getInstance(200, 300, "coin");
     Speed speed = Speed.getInstance(200, 300, "speed");
+
+    Sword sword =
 
 
     @Test
@@ -49,5 +52,15 @@ public class Sprint5UnitTests {
         CollisionManager.getInstance().addCollision(((Heart) heart).getCollisionBox());
         assertEquals(false, player.attemptMove(100, 500, 1));
         assertEquals(false, player.getPlayerHealth() > health);
+    }
+
+    @Test
+    public void testCoinLayer() {
+        assertEquals(coin.getLayer(), 1);
+    }
+
+    @Test
+    public void testHeartLayer() {
+        assertEquals(heart.getLayer(), 1);
     }
 }
